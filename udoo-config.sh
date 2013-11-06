c79feecb79a5cea77a705b9a7575d6a0acaba448
ektor5 <ek5.chimenti@gmail.com>
2013-11-06 15:13:15 +0100

    Fixed minor bug on ch_host

--- udoo-config.sh
+++ udoo-config.sh
@@ -56,9 +56,11 @@ ch_passwd()
 ch_host()
 {
   UDOO_OLD=`cat /etc/hostname`
-  UDOO_NEW=`$D --title="$TITLE" --entry --text="Enter hostname (current: $UDOO_OLD)" | tr -d " \t\n\r" `
+  UDOO_NEW=`$D --title="$TITLE" --entry --text="Enter hostname (current: $UDOO_OLD)" `
   
   (( $? )) && exit 1
+  
+  UDOO_NEW=`echo $UDOO_NEW | tr -d " \t\n\r" `
   
   [[ -z $UDOO_NEW ]] && error "Hostname cannot be empty"
  
@@ -79,60 +81,60 @@ ch_host()
   [[ "$(cat /etc/hostname)" =~ 	"$UDOO_NEW" ]] || error
 
   ok "Success! (New hostname: $UDOO_NEW)
-  Please reboot!"
+Please reboot!"
 }
 
 mem_split()
 {
-	declare -i FBMEM GPUMEM
+  declare -i FBMEM GPUMEM
 
-	UDOO_ENV=`$PRINTENV 2>&1`
+  UDOO_ENV=`$PRINTENV 2>&1`
 
-	case $? in
-		1)  	error "$UDOO_ENV" ;;
-		127)	error "$PRINTENV not found" ;;
-	esac
+  case $? in
+	  1)  	error "$UDOO_ENV" ;;
+	  127)	error "$PRINTENV not found" ;;
+  esac
 
-	FBMEM=`echo $UDOO_ENV | grep \^memory | sed -n -e 's/memory.*fbmem\=\([0-9]*\)M.*/\1/p'`
-	GPUMEM=`echo $UDOO_ENV | grep \^memory | sed -n -e 's/memory.*gpumem\=\([0-9]*\)M.*/\1/p'`
+  FBMEM=`echo $UDOO_ENV | grep \^memory | sed -n -e 's/memory.*fbmem\=\([0-9]*\)M.*/\1/p'`
+  GPUMEM=`echo $UDOO_ENV | grep \^memory | sed -n -e 's/memory.*gpumem\=\([0-9]*\)M.*/\1/p'`
 
-	(( $FBMEM )) || FBMEM=24
-	(( $GPUMEM )) || GPUMEM=128
+  (( $FBMEM )) || FBMEM=24
+  (( $GPUMEM )) || GPUMEM=128
 
-	FBMEM=`$D --title="$TITLE" \
-			--width=400 \
-			--height=300 \
-			--scale \
-			--text="Choose a memory value for framebuffer (MB):" \
-			--value=$FBMEM \
-			--min-value=8 \
-			--max-value=256 \
-			`
-	(( $? )) && exit 1
+  FBMEM=`$D --title="$TITLE" \
+		  --width=400 \
+		  --height=300 \
+		  --scale \
+		  --text="Choose a memory value for framebuffer (MB):" \
+		  --value=$FBMEM \
+		  --min-value=8 \
+		  --max-value=256 \
+		  `
+  (( $? )) && exit 1
 
-	GPUMEM=`$D --title="$TITLE" \
-			--width=400 \
-			--height=300 \
-			--scale \
-			--text="Choose a memory value for video card (MB):" \
-			--value=$GPUMEM \
-			--min-value=8 \
-			--max-value=256 \
-			`
-	(( $? )) && exit 1 	 
+  GPUMEM=`$D --title="$TITLE" \
+		  --width=400 \
+		  --height=300 \
+		  --scale \
+		  --text="Choose a memory value for video card (MB):" \
+		  --value=$GPUMEM \
+		  --min-value=8 \
+		  --max-value=256 \
+		  `
+  (( $? )) && exit 1 	 
 
-	$SETENV memory fbmem=${FBMEM}M gpumem=${GPUMEM}M || error
+  $SETENV memory fbmem=${FBMEM}M gpumem=${GPUMEM}M || error
 
-	ok "Success! (FBMEM=${FBMEM}M GPUMEM=${GPUMEM}M)"
+  ok "Success! (FBMEM=${FBMEM}M GPUMEM=${GPUMEM}M)"
 }
 
 print_env()
 {
-	UDOO_ENV=`$PRINTENV 2>&1`
+  UDOO_ENV=`$PRINTENV 2>&1`
 
-	(( $? )) && error "$UDOO_ENV"
+  (( $? )) && error "$UDOO_ENV"
 
-	$PRINTENV 2>&1 | $D --title="$TITLE" --text-info --font="monospace,9"
+  $PRINTENV 2>&1 | $D --title="$TITLE" --text-info --font="monospace,9"
 }
 
 ntpdate_rtc()
@@ -186,7 +188,7 @@ do
 	  0		4		"Memory Split" \
 	  0		5		"Show u-boot Environment" \
 	  0		6		"Update date from network and sync with RTC" \
-		0		9		"Credits" \
+	  0		9		"Credits" \
 	      `
   EXIT=$?
 	      
@@ -204,7 +206,7 @@ do
     
     6) (ntpdate_rtc) ;;
 
-		9) (credits) ;;	
+    9) (credits) ;;	
   esac
 
 done
